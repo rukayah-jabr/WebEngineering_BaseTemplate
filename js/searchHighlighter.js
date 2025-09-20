af51b8b5be09fe55c537dc7c78c15a8dc4cc4837
rukayah-jabr <rukayah.jabr@stud.fh-campuswien.ac.at>
2025-09-20 16:25:29 +0200

    corrected searchHighlighter

--- js/searchHighlighter.js
+++ js/searchHighlighter.js
@@ -1,35 +1,43 @@
 // Search highlighter
 export const initSearchHighlighter = () => {
-    document.querySelector('.search').addEventListener('submit', (e) => {
-        e.preventDefault();
+    try {
+        const form = document.querySelector('.search');
+        if (!form) return;
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
 
-        document.querySelectorAll('.highlight').forEach(function(el) {
-          const parent = el.parentNode;
-          parent.replaceChild(document.createTextNode(el.textContent), el);
-          parent.normalize();
+            document.querySelectorAll('.highlight').forEach((el) => {
+                const parent = el.parentNode;
+                parent.replaceChild(document.createTextNode(el.textContent), el);
+                parent.normalize();
+            });
+
+            const searchKey = e.target.elements.q?.value.trim();
+            if (!searchKey) return;
+
+            const regex = new RegExp('(' + searchKey.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + ')', 'gi');
+
+            const walk = (node) => {
+                if (node.nodeType === 3) {
+                    const match = node.nodeValue.match(regex);
+                    if (match) {
+                        const span = document.createElement('span');
+                        span.innerHTML = node.nodeValue.replace(regex, '<mark class="highlight">$1</mark>');
+                        node.replaceWith(...span.childNodes);
+                    }
+                }
+                else if (node.nodeType === 1 && node.tagName !== 'SCRIPT' && node.tagName !== 'STYLE' && node.tagName !== 'FORM') {
+                    node.childNodes.forEach(walk);
+                }
+            }
+
+            walk(document.body);
         });
 
-        const searchKey = this.q.value.trim();
-        if (!searchKey) return;
+    } catch (err) {
+        console.error("Search highlighter error:", err);
+        alert("Something went wrong with the search highlighter.");
+    }
 
-        const regex = new RegExp('(' + searchKey.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + ')', 'gi');
-
-        const walk = (node) => {
-          if (node.nodeType === 3) { // Text node
-            const match = node.nodeValue.match(regex);
-            if (match) {
-              const span = document.createElement('span');
-              span.innerHTML = node.nodeValue.replace(regex, '<mark class="highlight">$1</mark>');
-              node.replaceWith(...span.childNodes);
-            }
-          } 
-          else if (node.nodeType === 1 && node.tagName !== 'SCRIPT' && node.tagName !== 'STYLE' && node.tagName !== 'FORM') {
-            node.childNodes.forEach(walk);
-          }
-        }
-
-        walk(document.body);
-      });
 
 }
-      
