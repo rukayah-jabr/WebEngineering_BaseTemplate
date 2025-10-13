02945bdf03282b6ccc99f94edba22a68d9cf8c0d
rukayah-jabr <rukayah.jabr@stud.fh-campuswien.ac.at>
2025-10-13 17:34:38 +0200

    test für pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
     root: ".",
     base: "/WebEngineering_BaseTemplate/",   //für Pages
+    build: {
+    outDir: "dist" // Standard ist "dist", optional
+    },
     server: {
         open: true        // auto-open browser
     }
